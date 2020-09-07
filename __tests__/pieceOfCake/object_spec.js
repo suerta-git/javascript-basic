2fd4b277e48aa65168be0376ad08edcd576e4e5c
Suerta <suerta__@hotmail.com>
2020-09-07 17:37:51 +0800

    [Zhang Jindong] feat: should parse JSON to object

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -94,7 +94,10 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = {
+      color: 'Red',
+      value: '#ff0000',
+    };
     // --end->
 
     expect(color).toEqual(expected);
