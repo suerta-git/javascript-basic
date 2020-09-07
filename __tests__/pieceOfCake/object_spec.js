4282c44e14c2b02414aec1d8ac4265a91e13ceb2
Suerta <suerta__@hotmail.com>
2020-09-07 18:07:59 +0800

    [Zhang Jindong] feat: should get object prototype for an object

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -153,7 +153,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = Object.prototype;
     // --end->
 
     expect(Object.getPrototypeOf(emptyObject)).toBe(expected);
