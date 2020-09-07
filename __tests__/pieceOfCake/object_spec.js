de23d7404929afde595673a902ce44ce9f2bbda8
Suerta <suerta__@hotmail.com>
2020-09-07 18:12:12 +0800

    [Zhang Jindong] feat: should get null for object prototype's prototype

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -164,7 +164,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = null;
     // --end->
 
     expect(Object.getPrototypeOf(objectPrototype)).toEqual(expected);
