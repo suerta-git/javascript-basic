fc95695d7ae26bba77f5fb7deb342271341522fd
Suerta <suerta__@hotmail.com>
2020-09-07 17:33:55 +0800

    [Zhang Jindong] feat: should destruct object

--- __tests__/pieceOfCake/object_spec.js
+++ __tests__/pieceOfCake/object_spec.js
@@ -70,7 +70,7 @@ describe('for object', () => {
 
     // <--start
     // Please write down the correct value. You should write the final result directly.
-    const expected = undefined;
+    const expected = 'Bob';
     // --end->
 
     expect(name).toEqual(expected);
